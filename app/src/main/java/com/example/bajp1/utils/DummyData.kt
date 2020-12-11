76e60f8d805b1dcffbf0c61cd14ad6aa1f32e941
wirya <abrarwiryawan@gmail.com>
2020-12-11 22:54:46 +0700

    Add dummy series

--- app/src/main/java/com/example/bajp1/utils/DummyData.kt
+++ app/src/main/java/com/example/bajp1/utils/DummyData.kt
@@ -4,7 +4,7 @@ import com.example.bajp1.data.MovieEntity
 
 object DummyData {
 
-    fun generateMovie(): List<MovieEntity> {
+    fun generateMovies(): List<MovieEntity> {
 
         val movies = ArrayList<MovieEntity>()
 
@@ -79,5 +79,80 @@ object DummyData {
         ))
 
         return movies
+    }
+
+    fun generateSeries(): List<MovieEntity> {
+        val series = ArrayList<MovieEntity>()
+
+        series.add(MovieEntity(
+                "tv0", "Marvel's Daredevil", "79%", "Action, Crime",
+                "Lawyer-by-day Matt Murdock uses his heightened senses from being blinded as a young boy to fight crime at night on the streets of Hell’s Kitchen as Daredevil.",
+                "https://image.tmdb.org/t/p/w1280/QWbPaDxiB6LW2LjASknzYBvjMj.jpg",
+                "2015", "52m"
+        ))
+
+        series.add(MovieEntity(
+                "tv1", "The Good Doctor", "86%", "Drama",
+                "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives?",
+                "https://image.tmdb.org/t/p/w1280/6tfT03sGp9k4c0J3dypjrI8TSAI.jpg",
+                "2017", "42m"
+        ))
+
+        series.add(MovieEntity(
+                "tv2","Lucifer", "85%", "Crime, Sci-Fi & Fantasy",
+                "Bored and unhappy as the Lord of Hell, Lucifer Morningstar abandoned his throne and retired to Los Angeles, where he has teamed up with LAPD detective Chloe Decker to take down criminals. But the longer he's away from the underworld, the greater the threat that the worst of humanity could escape.",
+                "https://image.tmdb.org/t/p/w1280/4EYPN5mVIhKLfxGruy7Dy41dTVn.jpg",
+                "2016", "45m"
+        ))
+
+        series.add(MovieEntity(
+                "tv3", "Game of Thrones", "83%", "Sci-Fi & Fantasy, Drama, Action & Adventure, Mystery",
+                "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+                "https://image.tmdb.org/t/p/w1280/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg",
+                "2011", "1h"
+        ))
+
+        series.add(MovieEntity(
+                "tv4", "The Boys", "85%", "Sci-Fi & Fantasy, Action & Adventure",
+                "A group of vigilantes known informally as “The Boys” set out to take down corrupt superheroes with no more than blue-collar grit and a willingness to fight dirty.",
+                "https://image.tmdb.org/t/p/w1280/mY7SeH4HFFxW1hiI6cWuwCRKptN.jpg",
+                "2019", "1h"
+        ))
+        series.add(MovieEntity(
+                "tv5", "The Flash", "76%", "Drama, Sci-Fi & Fantasy",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \"meta-human\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "https://image.tmdb.org/t/p/w1280/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
+                "2014", "44m"
+        ))
+
+        series.add(MovieEntity(
+                "tv6", "Money Heist", "83%", "Crime, Drama",
+                "To carry out the biggest heist in history, a mysterious man called The Professor recruits a band of eight robbers who have a single characteristic: none of them has anything to lose. Five months of seclusion - memorizing every step, every detail, every probability - culminate in eleven days locked up in the National Coinage and Stamp Factory of Spain, surrounded by police forces and with dozens of hostages in their power, to find out whether their suicide wager will lead to everything or nothing.",
+                "https://image.tmdb.org/t/p/w1280/MoEKaPFHABtA1xKoOteirGaHl1.jpg",
+                "2017", "1h 10m"
+        ))
+
+        series.add(MovieEntity(
+                "tv7", "Stranger Things", "86%", "Mystery, Sci-Fi & Fantasy, Drama",
+                "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
+                "https://image.tmdb.org/t/p/w1280/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
+                "2016", "50m"
+        ))
+
+        series.add(MovieEntity(
+                "tv8", "Marvel's Agents of S.H.I.E.L.D.", "73%","Drama, Sci-Fi & Fantasy, Action & Adventure",
+                "Agent Phil Coulson of S.H.I.E.L.D. (Strategic Homeland Intervention, Enforcement and Logistics Division) puts together a team of agents to investigate the new, the strange and the unknown around the globe, protecting the ordinary from the extraordinary.",
+                "https://image.tmdb.org/t/p/w1280/gHUCCMy1vvj58tzE3dZqeC9SXus.jpg",
+                "2013", "43m"
+        ))
+
+        series.add(MovieEntity(
+                "tv9", "The Mandalorian", "85%", "Sci-Fi & Fantasy, Action & Adventure, Western",
+                "After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.",
+                "https://image.tmdb.org/t/p/w1280/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg",
+                "2019", "35m"
+        ))
+
+        return series
     }
 }
